e80b962dd1a833008c1ae62fa76fe5ab79f36478
Pranavvks <pranav.vks@gmail.com>
2022-01-01 20:37:50 +0600

    first commit

---
+++ src/recursion25/PallindromePartioning.java
@@ -0,0 +1,9 @@
+package recursion25;
+
+public class PallindromePartioning {
+    public static void main(String[] args) {
+        String str = "aab" ;
+        System.out.println(str.substring(2,3));
+//        System.out.println(str.length());
+    }
+}
